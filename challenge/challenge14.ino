dd1f0ea66fb6b672e658da6e78008af035c55ebb
yulian <yulian@naver.com>
2020-09-05 12:41:03 +0900

    Create challenge14.ino
    Added a new file for challenge 14

---
+++ challenge/challenge14.ino
@@ -0,0 +1,85 @@
+const int melody[] = {      // 음계 별 주파수를 저장한 변수 선언
+  262,	// '도'의 주파수
+  294,	// '레'의 주파수
+  330,	// '미'의 주파수
+  349,	// '파'의 주파수
+  392,	// '솔'의 주파수
+  440,	// '라'의 주파수
+  494,	// '시'의 주파수
+  523,	// '도'의 주파수
+};
+
+const int PIEZO_PIN = 8;  	// 피에조 스피커에 연결된 디지털 핀 번호
+const int BASE = 400;		    // 기본음 길이
+
+void setup()
+{
+  // 솔솔솔파미
+  tone_with_delay(melody[4], BASE, 200);
+  tone_with_delay(melody[4], BASE, 200);
+  tone_with_delay(melody[4], BASE, 200);
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[2], BASE*4, 200);
+  
+  delay(BASE*2);
+  
+  // 파파파미레
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[1], BASE*4, 200);
+  
+  delay(BASE*2);
+  
+  // 미미미파미도레
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[0], BASE, 200);
+  tone_with_delay(melody[1], BASE*6, 200);
+  
+  delay(BASE*2);
+  
+  // 솔솔솔파미
+  tone_with_delay(melody[4], BASE, 200);
+  tone_with_delay(melody[4], BASE, 200);
+  tone_with_delay(melody[4], BASE, 200);
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[2], BASE*4, 200);
+  
+  delay(BASE*2);
+  
+  // 파파파미레
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[1], BASE*4, 200);
+  
+  delay(BASE*2);
+  
+  // 미미미파미레도
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[3], BASE, 200);
+  tone_with_delay(melody[2], BASE, 200);
+  tone_with_delay(melody[1], BASE, 200);
+  tone_with_delay(melody[0], BASE*6, 200);
+}
+
+void loop()
+{
+
+}
+
+// 음의 지속시간과 간격을 주기위한 함수
+void tone_with_delay(int frequency, int duration, int ms)
+{
+  tone(PIEZO_PIN, frequency, duration);
+  delay(ms);
+}
+
