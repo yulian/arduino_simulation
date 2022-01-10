98ddbba64ba44c3ecf50728a2a0c3dd8f64c1247
yulian <yulian@naver.com>
2022-01-10 20:43:19 +0900

    Update challenge14.ino
    Change the delay after tone function is called (30% more from duration)

--- challenge/challenge14.ino
+++ challenge/challenge14.ino
@@ -10,65 +10,65 @@ const int melody[] = {      // 음계 별 주파수를 저장한 변수 선언
 };
 
 const int PIEZO_PIN = 8;    // 피에조 스피커에 연결된 디지털 핀 번호
-const int BASE = 400;       // 기본음 길이
+const int BASE = 200;       // 기본음 길이
 
 void setup()
 {
   // 솔솔솔파미
-  tone_with_delay(melody[4], BASE, 200);
-  tone_with_delay(melody[4], BASE, 200);
-  tone_with_delay(melody[4], BASE, 200);
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[2], BASE*4, 200);
+  tone_with_delay(melody[4], BASE);
+  tone_with_delay(melody[4], BASE);
+  tone_with_delay(melody[4], BASE);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[2], BASE*4);
   
   delay(BASE*2);
   
   // 파파파미레
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[1], BASE*4, 200);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[1], BASE*4);
   
   delay(BASE*2);
   
   // 미미미파미도레
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[0], BASE, 200);
-  tone_with_delay(melody[1], BASE*6, 200);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[0], BASE);
+  tone_with_delay(melody[1], BASE*6);
   
   delay(BASE*2);
   
   // 솔솔솔파미
-  tone_with_delay(melody[4], BASE, 200);
-  tone_with_delay(melody[4], BASE, 200);
-  tone_with_delay(melody[4], BASE, 200);
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[2], BASE*4, 200);
+  tone_with_delay(melody[4], BASE);
+  tone_with_delay(melody[4], BASE);
+  tone_with_delay(melody[4], BASE);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[2], BASE*4);
   
   delay(BASE*2);
   
   // 파파파미레
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[1], BASE*4, 200);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[1], BASE*4);
   
   delay(BASE*2);
   
   // 미미미파미레도
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[3], BASE, 200);
-  tone_with_delay(melody[2], BASE, 200);
-  tone_with_delay(melody[1], BASE, 200);
-  tone_with_delay(melody[0], BASE*6, 200);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[3], BASE);
+  tone_with_delay(melody[2], BASE);
+  tone_with_delay(melody[1], BASE);
+  tone_with_delay(melody[0], BASE*6);
 }
 
 void loop()
@@ -77,9 +77,9 @@ void loop()
 }
 
 // 음의 지속시간과 간격을 주기위한 함수
-void tone_with_delay(int frequency, int duration, int ms)
+void tone_with_delay(int frequency, int duration)
 {
   tone(PIEZO_PIN, frequency, duration);
-  delay(ms);
+  delay(duration*1.3);
 }
 
